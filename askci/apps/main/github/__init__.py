91d7589d619ec2e1f589824062bac981776327de
Vanessa Sochat <vsochat@stanford.edu>
2019-11-28 17:49:11 -0700

    adding github views, add a question page
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

---
+++ askci/apps/main/github/__init__.py
@@ -0,0 +1,419 @@
+"""
+
+Copyright (C) 2016-2020 Vanessa Sochat.
+
+This Source Code Form is subject to the terms of the
+Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
+
+import django_rq
+
+from askci.apps.main.tasks import update_article
+from askci.apps.users.models import User
+from askci.apps.main.models import Article
+from askci.settings import DISABLE_WEBHOOKS, DOMAIN_NAME
+
+from .utils import (
+    check_headers,
+    get_default_headers,
+    JsonResponseMessage,
+    load_body,
+    paginate,
+    validate_payload,
+    DELETE,
+    POST,
+)
+
+from dateutil.parser import parse
+from datetime import datetime
+import re
+import requests
+import uuid
+
+api_base = "https://api.github.com"
+
+
+## Authentication
+
+
+def get_auth(user, headers=None, idx=0):
+    """get_auth will return the authentication header for a user
+       the default headers (without auth) are returned if provider not github
+
+       Parameters
+       ==========
+       user: a user object
+    """
+    if headers is None:
+        headers = get_default_headers()
+
+    # Tasks might provide a user id instead
+    if not isinstance(user, User):
+        try:
+            user = User.objects.get(id=user)
+        except User.DoesNotExist:
+            pass
+
+    token = get_auth_token(user)
+
+    if token is not None:
+        token = "token %s" % (token)
+        headers["Authorization"] = token
+    return headers
+
+
+def get_auth_token(user):
+    """get_auth_token will return the auth token for a user.
+
+       Parameters
+       ==========
+       user: a user object
+    """
+    # 1. Github with repo permissions first priority
+    auth = [x for x in user.social_auth.all() if x.provider == "github"]
+
+    # 2. Github public second priority
+    if not auth:
+        auth = [x for x in user.social_auth.all() if x.provider == "github-readonly"]
+
+    if auth:
+        return auth[0].access_token
+
+
+# Repositories
+
+
+def get_repo(user, reponame, username, headers=None):
+    """get_repo will return a single repo, username/reponame
+       given authentication with user
+
+       Parameters
+       ==========
+       user: the user to get github credentials for
+       reponame: the name of the repo to retrieve
+       username: the username of the repo (owner)
+    """
+    # Case 1, the user just has one auth or just public
+    if headers is None:
+        headers = get_auth(user)
+    headers["Accept"] = "application/vnd.github.mercy-preview+json"
+    url = "%s/repos/%s/%s" % (api_base, username, reponame)
+    response = requests.get(url, headers=headers)
+
+    # Case 2: public and private
+    if response.status_code != 200:
+        auth_headers = get_auth(user, idx=1)
+        headers.update(auth_headers)
+        response = requests.get(url, headers=headers)
+    response = response.json()
+    return response
+
+
+def get_namespaces(user):
+    """for a given user, list the organizations and username (namespaces)
+       they have create access to.
+    """
+    namespaces = []
+    if not user.has_github_create():
+        return namespaces
+
+    headers = get_auth(user)
+
+    namespaces = [user.username]
+    # this url is supposed to return public/private but doens't seem to work
+    # url = "%s/user/orgs" % api_base
+    url = "%s/users/%s/orgs" % (api_base, user.username)
+    response = requests.get(url, headers=headers)
+
+    # User can create for personal repo, not org
+    if response.status_code in [401, 403]:
+        return namespaces
+
+    response = response.json()
+
+    # No orgs to add
+    if not response:
+        return namespaces
+
+    namespaces = namespaces + [x["login"] for x in response]
+    return namespaces
+
+
+def open_issue(user, article, title, summary):
+    """given a user with GitHub permissions, an article, and markdown, dispatch an action
+       to generate a pull request and return the link to the user.
+    """
+    # User must be authenticated with GitHub create
+    if not user.has_github_create():
+        return
+
+    headers = get_auth(user)
+    headers["Accept"] = "application/vnd.github.symmetra-preview+json"
+    data = {"title": title, "body": summary}
+
+    # Data should include updated markdown (for README)
+    url = "%s/repos/%s/issues" % (api_base, article.repo["full_name"])
+    response = requests.post(url, headers=headers, json=data)
+
+    return response.json()
+
+
+def request_review(user, article, markdown):
+    """given a user with GitHub permissions, an article, and markdown, dispatch an action
+       to generate a pull request and return the link to the user.
+    """
+    # Must be authenticated with GitHub create? (need to check this)
+    if not article.owner.has_github_create():
+        return
+
+    # Replace all "\r\n" with just \n
+    markdown = markdown.replace("\r\n", "\n")
+    headers = get_auth(article.owner)
+    headers["Accept"] = "application/vnd.github.everest-preview+json"
+
+    pr_id = str(uuid.uuid4())
+    callback_url = "%s%s/" % (DOMAIN_NAME.strip("/"), reverse("receive_pr_request"))
+    data = {
+        "event_type": "request_review_by_%s" % user.username,
+        "client_payload": {
+            "markdown": markdown,
+            "username": user.username,
+            "url": callback_url,
+            "article": str(article.uuid),
+            "id": pr_id,
+        },
+    }
+
+    # Data should include updated markdown (for README)
+    url = "%s/repos/%s/dispatches" % (api_base, article.repo["full_name"])
+    response = requests.post(url, headers=headers, json=data)
+    return response.status_code, pr_id
+
+
+def fork_repository(user, template, repository=None):
+    """Fork a repository. If repository is defined, rename to this
+
+       Parameters
+       ==========
+       user: the user to request permission to fork. Must be connected to the 
+             GitHub API.
+       template: the template repository name (https://github.com/<user>/<repo>
+       repository: rename the fork to this <user>/<repo>
+    """
+    repo = None
+    if user.has_github_create():
+        headers = get_auth(user)
+        headers["Accept"] = "application/vnd.github.baptiste-preview+json"
+
+        repo_owner, repo_name = template.split("/")[-2:]
+
+        # Default data is None
+        data = None
+
+        # If the repository is to be renamed
+        if repository is not None:
+            owner, term_name = repository.split("/")
+
+            # If the owner isn't the user, we need to specify data
+            if owner != user.username:
+                data = {"organization": owner}
+
+        url = "%s/repos/%s/%s/forks" % (api_base, repo_owner, repo_name)
+        response = requests.post(url, headers=headers, data=data)
+
+        # Success?
+        if response.status_code in [200, 202, 201]:
+            repo = response.json()
+
+            # Rename it with the edit api, and ensure has issues
+            updates = {"has_issues": True}
+            if repository is not None:
+                updates["name"] = term_name
+
+            url = "%s/repos/%s/%s" % (api_base, owner, repo_name)
+            response = requests.patch(url, headers=headers, json=updates)
+            if response.status_code == 200:
+                repo = response.json()
+
+    return repo
+
+
+def copy_repository_template(user, template, repository):
+    """create a repository from a template, and return content to connect
+       to an article. Returns None if not possible. NOTE: this is not
+       currently working - the API returns 404 when it should not.
+    """
+    if user.has_github_create():
+        headers = get_auth(user)
+        headers["Accept"] = "application/vnd.github.baptiste-preview+json"
+
+        template_owner, template_repo = template.split("/")[-2:]
+        owner, repo = repository.split("/")
+
+        data = {
+            "owner": owner,
+            "name": repo,
+            "description": "Documentation repository for AskCI",
+        }
+        url = "%s/repos/%s/%s/generate" % (api_base, template_owner, template_repo)
+        response = requests.post(url, headers=headers, data=data)
+
+
+def list_repos(user, headers=None):
+    """list_repos will list the public repos for a user
+
+       Parameters
+       ==========
+       user: a user object to list
+       headers: headers to replace default
+    """
+    if headers is None:
+        headers = get_auth(user)
+    url = "%s/user/repos" % (api_base)
+    repos = paginate(url=url, headers=headers)
+
+    if not repos:
+        auth_headers = get_auth(user, idx=1)
+        headers.update(auth_headers)
+        repos = paginate(url=url, headers=headers)
+    return repos
+
+
+# Webhooks
+
+
+def create_webhook(user, repo, secret):
+    """create_webhook will create a webhook for a repo to send back
+       to askci on push.
+
+       Parameters
+       ==========
+       user: user: should be an askci user with GitHub authentication.
+       repo: should be a complete repo object, including username and reponame
+       secret: should be a randomly generated string, created when repo connected,
+               to validate future pushes
+    """
+    if user.has_github_create():
+        headers = get_auth(user)
+        url = "%s/repos/%s/hooks" % (api_base, repo["full_name"])
+        callback_url = "%s%s/" % (DOMAIN_NAME.strip("/"), reverse("receive_hook"))
+        config = {"url": callback_url, "content_type": "json", "secret": secret}
+
+        params = {
+            "name": "web",
+            "active": True,
+            "events": ["push", "deployment"],
+            "config": config,
+        }
+
+        # Create webhook
+        response = POST(url, headers=headers, data=params)
+        response = response.json()
+
+        return response
+
+
+## Delete
+
+
+def delete_webhook(user, repo, hook_id):
+    """delete_webhook will delete a webhook, done when a user deletes a collection.
+       https://developer.github.com/v3/repos/hooks/#delete-a-hook
+       DELETE /repos/:owner/:repo/hooks/:hook_id
+
+       Parameters
+       ==========
+       user: should be a singularity hub user. This is used to get
+             the Github authentication
+       repo: should be a complete repo object, including username and reponame
+    """
+    if user.has_github_create():
+        headers = get_auth(user)
+        url = "%s/repos/%s/hooks/%s" % (api_base, repo, hook_id)
+
+        response = DELETE(url, headers)
+        return response.json()
+
+
+# Receive GitHub webhook
+
+
+@csrf_exempt
+def receive_github_hook(request):
+    """a hook is sent on each push event, meaning that the content for
+       the repository has been updated (and has passed tests). When this
+       happens, we 
+    """
+    # We do these checks again for sanity
+    if request.method == "POST":
+
+        if DISABLE_WEBHOOKS:
+            return JsonResponseMessage(message="Webhooks disabled")
+
+        if not re.search("GitHub-Hookshot", request.META["HTTP_USER_AGENT"]):
+            return JsonResponseMessage(message="Agent not allowed")
+
+        # Only allow application/json content type
+        if request.META["CONTENT_TYPE"] != "application/json":
+            return JsonResponseMessage(message="Incorrect content type")
+
+        # Check that it's coming from the right place
+        required_headers = ["HTTP_X_GITHUB_DELIVERY", "HTTP_X_GITHUB_EVENT"]
+        if not check_headers(request, required_headers):
+            return JsonResponseMessage(message="Agent not allowed")
+
+        # Has to be a push or deployment
+        if request.META["HTTP_X_GITHUB_EVENT"] not in ["push", "deployment"]:
+            return JsonResponseMessage(message="Incorrect delivery method.")
+
+        # A signature is also required
+        signature = request.META.get("HTTP_X_HUB_SIGNATURE")
+        if not signature:
+            return JsonResponseMessage(message="Missing credentials.")
+
+        # Parse the body
+        payload = load_body(request)
+        repo = payload.get("repository")
+
+        import pickle
+
+        pickle.dump(payload, open("payload.pkl", "wb"))
+
+        # Retrieve the article (need to test this)
+        try:
+            article = Article.objects.get(repo__full_name=repo["full_name"])
+        except Article.DoesNotExist:
+            return JsonResponseMessage(message="Article not found", status=404)
+
+        # Validate the payload with the collection secret
+        status = validate_payload(
+            secret=article.secret, payload=request.body, request_signature=signature
+        )
+
+        if not status:
+            return JsonResponseMessage(message="Invalid credentials.")
+
+        # Branch must be master
+        branch = payload.get("ref", "refs/heads/master").replace("refs/heads/", "")
+        if branch != "master":
+            return JsonResponseMessage(message="Branch must be master.")
+
+        # Update repo metadata that might change
+        article.repo = repo
+        article.save()
+
+        # TODO figure out what need from payload to update article
+
+        # Submit job with django_rq to update article
+        res = django_rq.enqueue(update_article, article_uuid=article.uuid)
+
+        return JsonResponseMessage(
+            message="Hook received and parsing.", status=200, status_message="Received"
+        )
+
+    return JsonResponseMessage(message="Invalid request.")
--- askci/apps/main/views/questions.py
+++ askci/apps/main/views/questions.py
@@ -14,21 +14,28 @@ from django.shortcuts import render, redirect
 from django.http import Http404, JsonResponse
 from ratelimit.decorators import ratelimit
 
+from askci.settings import DOMAIN_NAME
 from askci.apps.main.models import Article, Question
-from askci.apps.main.utils import lowercase_cleaned_name
 from askci.apps.main.tasks import update_article
 from askci.settings import VIEW_RATE_LIMIT as rl_rate, VIEW_RATE_LIMIT_BLOCK as rl_block
-from askci.apps.main.github import (
-    get_namespaces,
-    fork_repository,
-    create_webhook,
-    request_review,
-)
+from askci.apps.main.github import open_issue
 
 import os
 import uuid
 
 ## All Questions view is shown on front page in base -> main/index.html
+
+
+def get_article(name):
+    """a general function to get an article, returns None if doesn't exist
+    """
+    article = None
+    if name is not None:
+        try:
+            article = Article.objects.get(name=name)
+        except Article.DoesNotExist:
+            pass
+    return article
 
 
 @ratelimit(key="ip", rate=rl_rate, block=rl_block)
@@ -38,25 +45,41 @@ def new_question(request, name=None):
        to selecting an article (associated with a repository) and then
        opening an issue on the GitHub board.
     """
-    article = None
 
     # If a name is given the user has already chosen an article
-    if name is not None:
-        try:
-            article = Article.objects.get(name=name)
-        except Article.DoesNotExist:
-            messages.info(request, "We couldn't find article '%s'" % name)
+    article = get_article(name)
+    if not article:
+        messages.info(request, "We couldn't find article '%s'" % name)
 
     context = {"article": article, "articles": Article.objects.order_by("-name")}
 
     if request.method == "POST":
-        article = request.POST.get("article")
+
+        name = request.POST.get("article")
         summary = request.POST.get("summary")
         title = request.POST.get("title")
-        print(article)
-        print(summary)
-        print(title)
 
-        # TODO open issue on GitHub board here
-        messages.info(request, "An issue has been opened with your question!")
+        # Add link to the article to the summary
+        summary += "<br> %s/%s" % (DOMAIN_NAME.strip("/"), article.get_absolute_url())
+
+        article = get_article(name)
+        if not article:
+            messages.info(request, "We couldn't find article '%s'" % name)
+
+        # Open an issue with the question on the GitHub issues board
+        else:
+            response = open_issue(request.user, article, title, summary)
+            if response:
+                url = response.get("url", "#")
+                # Format the url to be a link
+                url = "<a href='%s' target='_blank'>your question!</a>" % url
+                messages.info(
+                    request, "An issue has been opened with your question %s" % url
+                )
+            else:
+                messages.info(
+                    request,
+                    "There was a problem posting your question, are you authenticated with GitHub?",
+                )
+
     return render(request, "questions/new_question.html", context)
